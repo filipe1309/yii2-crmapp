2e2aa8d2ff614334b4b78608e0eaf9a346d452b8
filipe1309 <filipe1309@outlook.com>
2018-03-21 16:32:16 +0000

    Add yii2 entry point
    Signed-off-by: filipe1309 <filipe1309@outlook.com>

--- web/index.php
+++ web/index.php
@@ -1,1 +1,7 @@
-Our CRM
+<?php
+// Including the Yii framework itsolf
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+// Getting hte configuration
+$config = require(__DIR__ . '/../config/web.php');
+// Making and launching the application immediatily
+(new yii\web\Application($config))->run();
