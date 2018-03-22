3d9da37861440b209b73132ffa725567361c03fb
filipe1309 <filipe1309@outlook.com>
2018-03-22 16:31:23 +0000

    Add yii2 debug mode
    Signed-off-by: filipe1309 <filipe1309@outlook.com>

--- web/index.php
+++ web/index.php
@@ -1,7 +1,15 @@
 <?php
+// Set debug mode
+define('YII_DEBUG', true);
+
 // Including the Yii framework itsolf
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+// Override display_errors
+ini_set('display_errors', true);
+
 // Getting hte configuration
 $config = require(__DIR__ . '/../config/web.php');
+
 // Making and launching the application immediatily
 (new yii\web\Application($config))->run();
