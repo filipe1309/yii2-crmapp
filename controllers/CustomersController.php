c90736bf338ccc922670d3f8da6226570dd0a57a
filipe1309 <filipe1309@outlook.com>
2018-03-23 17:16:52 +0000

    Add methods to decoupling from ORM
    Signed-off-by: filipe1309 <filipe1309@outlook.com>

--- controllers/CustomersController.php
+++ controllers/CustomersController.php
@@ -3,6 +3,10 @@
 namespace app\controllers;
 
 use yii\web\Controller;
+use app\models\customer\Customer;
+use app\models\customer\CustomerRecord;
+use app\models\customer\Phone;
+use app\models\customer\PhoneRecord;
 
 class CustomersController extends Controller 
 {
@@ -10,4 +14,50 @@ class CustomersController extends Controller
         $records = $this->getRecordsAccordingToQuery();
         $this->render('index', compact('records'));
     }
+    
+    /**
+     * Convert Customer model to CustomerRecord model and save in the database
+     * 
+     * @param Customer $customer
+     */
+    private function store(Customer $customer)
+    {
+        $customer_record = new CustomerRecord();
+        $customer_record->name = $customer->name;
+        $customer_record->birth_date = $customer->birth_date->format('Y-m-d');
+        $customer_record->notes = $customer->notes;
+        
+        $customer_record->save();
+        
+        foreach ($customer->phones as $phone) {
+            $phone_record = new PhoneRecord;
+            $phone_record->number = $phone->number;
+            $phone_record->customer_id = $customer_record->id;
+            $phone_record->save();
+        }
+    }
+    
+    /**
+     * Convert CustomerRecord model to Customer model and return the Customer
+     * 
+     * @param CustomerRecord $customer_record
+     * @param PhoneRecord $phone_record
+     * @return Customer
+     */
+    private function makeCustomer(
+        CustomerRecord $customer_record,
+        PhoneRecord $phone_record
+    )
+    {
+        $name = $customer_record->name;
+        $birth_date = new \DateTime($customer_record->birth_date);
+        
+        $customer = new Customer($name, $birth_date);
+        $customer->notes = $customer_record->notes;
+        $customer->phones[] = new Phone($phone_record->number);
+        
+        return $customer;
+    }
+    
+    
 }
