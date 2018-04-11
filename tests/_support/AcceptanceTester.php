bd61923fb3f1c08c07fb6ac738b54194ede5c3ae
filipe1309 <filipe1309@outlook.com>
2018-04-11 20:26:29 +0000

    Switch from assertGreaterThen deprecated method to assertGreaterThan
    Signed-off-by: filipe1309 <filipe1309@outlook.com>

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -35,6 +35,6 @@ class AcceptanceTester extends \Codeception\Actor
     
     public function seeContentIsLong($content, $trigger_length = 100)
     {
-        $this->assertGreaterThen($trigger_length, strlen($content));
+        $this->assertGreaterThan($trigger_length, strlen($content));
     }
 }
