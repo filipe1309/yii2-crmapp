1c075841f4b10c4aa76ec3131a6b20226a8d24a1
filipe1309 <filipe1309@gmail.com>
2018-04-20 19:34:06 +0000

    Add login/logout functionality to Guests users on tests
    Signed-off-by: filipe1309 <filipe1309@gmail.com>

--- tests/_support/Step/Acceptance/CRMGuestSteps.php
+++ tests/_support/Step/Acceptance/CRMGuestSteps.php
@@ -3,5 +3,37 @@ namespace Step\Acceptance;
 
 class CRMGuestSteps extends \AcceptanceTester
 {
-
+    public $username;
+    public $password;
+    
+    public function __construct($scenario)
+    {
+        parent::__construct($scenario);
+        
+        // Automatically login the user, if user and pass are set
+        if ($this->username and $this->password)
+            $this->login($this->username, $this->password);
+    }
+    
+    public function login($username, $password)
+    {
+        $I = $this;
+        $I->amOnPage('/site/login');
+        $I->fillField('LoginForm[username]', $username);
+        $I->fillField('LoginForm[password]', $password);
+        $I->click('Login');
+        $I->wait(1); // Wait because JS validation
+        $I->seeCurrentUrlEquals('/web_app_dev_yii2_php/yii2-crmapp/web/');
+    }
+    
+    public function logout()
+    {
+        $I = $this;
+        // Move to homepage, because it could be in an exception page
+        $I->amOnPage('/');
+        // Expecting that this button is presented on the homepage.
+        $I->click('logout');
+        //$I->wait(1);
+        //$I->see('guest');
+    }
 }
