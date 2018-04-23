968c0bf0d723a0be9c491739f7a82033553d9dbf
filipe1309 <filipe1309@gmail.com>
2018-04-23 19:25:13 +0000

    Add the 3rd module as a child of the 2nd module using the init method
    Signed-off-by: filipe1309 <filipe1309@gmail.com>

--- utilities/SecondModule.php
+++ utilities/SecondModule.php
@@ -3,4 +3,16 @@ namespace app\utilities;
 
 use yii\base\Module;
 
-class SecondModule extends Module {}
+class SecondModule extends Module
+{
+    public function init()
+    {
+        parent::init();
+        $this->modules = [
+            'thirdlevel' => [
+                'class' => 'app\utilities\ThirdModule',
+                'basePath' => '@app'
+            ]
+        ];
+    }
+}
