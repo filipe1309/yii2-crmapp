3c4f8ec71c5df9ae46f5db0717ee578eb1b63b90
filipe1309 <filipe1309@gmail.com>
2018-04-26 13:33:04 +0000

    Add an example of profiling
    Signed-off-by: filipe1309 <filipe1309@gmail.com>

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -4,6 +4,8 @@ namespace app\controllers;
 
 use Yii;
 use yii\web\Controller;
+use yii\web\Response;
+use yii\log\Logger;
 use app\models\user\LoginForm;
 
 class SiteController extends Controller
@@ -34,5 +36,31 @@ class SiteController extends Controller
     {
         Yii::$app->user->logout();
         return $this->goHome();
+    }
+    
+    public function actionProfile()
+    {
+        Yii::beginProfile('outer', 'beginning');
+        
+        Yii::getLogger()->log('first', Logger::LEVEL_PROFILE);
+        Yii::trace('second');
+        Yii::info('third');
+        
+        Yii::beginProfile('inner', 'beginning');
+        
+        Yii::warning('fourth', 'nonapplication');
+        Yii::info('fifth');
+        
+        Yii::endProfile('inner', 'ending');
+        
+        Yii::endProfile('outer', 'ending');
+        
+        $result = Yii::$app->response;
+        $result->data = Yii::getLogger()->getProfiling(
+            ['beginning', 'application', 'ending']
+        );
+        $result->format = Response::FORMAT_JSON;
+        
+        return $result;
     }
 }
