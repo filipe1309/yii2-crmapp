5975fa04dd8c7bbb65568028783c0aba2db29a2f
filipe1309 <filipe1309@gmail.com>
2018-05-03 14:32:19 +0000

    Create address CRUD
    Signed-off-by: filipe1309 <filipe1309@gmail.com>

---
+++ views/addresses/_form.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\customer\AddressRecord */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="address-record-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'purpose')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'country')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'state')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'street')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'building')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'apartment')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'received_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'postal_code')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'customer_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
