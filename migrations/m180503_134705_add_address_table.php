b36f9172361a2a783eee6b16d581216d58b92b6b
filipe1309 <filipe1309@gmail.com>
2018-05-03 13:57:32 +0000

    Create a migration to create address table
    Signed-off-by: filipe1309 <filipe1309@gmail.com>

---
+++ migrations/m180503_134705_add_address_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180503_134705_add_address_table
+ */
+class m180503_134705_add_address_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable(
+            'address',
+            [
+                'id' => 'string',
+                'purpose' => 'string',
+                'country' => 'string',
+                'state' => 'string',
+                'city' => 'string',
+                'street' => 'string',
+                'building' => 'string',
+                'apartment' => 'string',
+                'received_name' => 'string',
+                'postal_code' => 'string',
+                'customer_id' => 'int not null'
+            ]
+        );
+        
+        // add foreign key with name on table column referencing table column
+        $this->addForeignKey(
+            'customer_address', 
+            'address', 'customer_id', 
+            'customer', 'id'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('customer_address', 'address');
+        $this->dropTable('address');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180503_134705_add_address_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
