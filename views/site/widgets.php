4e38d4ea4955f4b2d94d0a7b22a640b84a46449f
filipe1309 <filipe1309@gmail.com>
2018-05-07 17:44:55 +0000

    Create a custom widget with begin/end behavior
    Signed-off-by: filipe1309 <filipe1309@gmail.com>

--- views/site/widgets.php
+++ views/site/widgets.php
@@ -1,6 +1,18 @@
 <?php
 use app\components\MyWidget;
+use app\components\MyWidgetBeginEnd;
 ?>
 
 <h1>MyWidget using ::widget() </h1>
 <?= MyWidget::widget(['message' => 'Hello friends!!!!']); ?>
+
+<br>
+
+<h1>MyWidgetBeginEnd using ::begin() ::end()</h1>
+<tag></tag>
+
+<?php MyWidgetBeginEnd::begin(); ?>
+
+content that may contain <tag>'s
+
+<?php MyWidgetBeginEnd::end(); ?>
