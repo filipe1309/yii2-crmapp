975110a21f30e5a98b7e80edeec3e317eabbc87d
filipe1309 <filipe1309@gmail.com>
2018-05-09 19:04:44 +0000

    Create the script to prepare the application in the guest OS of VM
    Signed-off-by: filipe1309 <filipe1309@gmail.com>

---
+++ bootstrap/local.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'components' => [
+        'db' => [
+            'dsn' => 'mysql:host=localhost;dbname=crmapp',
+            'username' => 'filipe1309',
+            'password' => ''
+        ]
+    ]
+];
