dcee74eaf9a5cb54bfd35d5b2c2596ddb1ec7bf8
filipe1309 <filipe1309@gmail.com>
2018-05-10 13:08:20 +0000

    Customize the autogenerated form of Customer Records
    Signed-off-by: filipe1309 <filipe1309@gmail.com>

--- views/customer-records/_form.php
+++ views/customer-records/_form.php
@@ -1,24 +1,89 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use yii\bootstrap\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\customer\CustomerRecord */
-/* @var $form yii\widgets\ActiveForm */
+/* @var $form yii\bootstrap\ActiveForm */
 ?>
 
 <div class="customer-record-form">
 
-    <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'id')->textInput() ?>
+    <?php $form = ActiveForm::begin(['layout' => 'horizontal']); ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'birth_date')->textInput() ?>
 
     <?= $form->field($model, 'notes')->textarea(['rows' => 6]) ?>
+    
+    <?php if (!$model->isNewRecord):?>
+        <h2>Phones</h2>
+        <?= \yii\grid\GridView::widget([
+                'dataProvider' => new \yii\data\ActiveDataProvider([
+                'query' => $model->getPhones(),
+                'pagination' => false
+            ]),
+            'columns' => [
+                'number',
+                [
+                    'class' => 'yii\grid\ActionColumn',
+                    'controller' => 'phones',
+                    'header' => Html::a(
+                        '<i class="glyphicon glyphicon-plus"></i>&nbsp;Add New',
+                        ['phones/create']
+                    ),
+                    'template' => '{update}{delete}',
+                ],
+            ]
+        ]);?>
+        
+        <h2>Addresses</h2>
+        <?= \yii\grid\GridView::widget([
+                'dataProvider' => new \yii\data\ActiveDataProvider([
+                'query' => $model->getAddresses(),
+                'pagination' => false
+            ]),
+            'columns' => [
+                'purpose',
+                'country',
+                'city',
+                'received_name',
+                'postal_code',
+                [
+                    'class' => 'yii\grid\ActionColumn',
+                    'controller' => 'addresses',
+                    'header' => Html::a(
+                        '<i class="glyphicon glyphicon-plus"></i>&nbsp;Add New',
+                        ['addresses/create']
+                    ),
+                    'template' => '{update}{delete}',
+                ],
+            ]
+        ]);?>
+        
+        <h2>Emails</h2>
+        <?= \yii\grid\GridView::widget([
+                'dataProvider' => new \yii\data\ActiveDataProvider([
+                'query' => $model->getEmails(),
+                'pagination' => false
+            ]),
+            'columns' => [
+                'address',
+                'purpose',
+                [
+                    'class' => 'yii\grid\ActionColumn',
+                    'controller' => 'emails',
+                    'header' => Html::a(
+                        '<i class="glyphicon glyphicon-plus"></i>&nbsp;Add New',
+                        ['emails/create']
+                    ),
+                    'template' => '{update}{delete}',
+                ],
+            ]
+        ]);?>
+    <?php endif?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
